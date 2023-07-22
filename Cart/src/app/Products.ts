c95822e29b1be503df13342bf215e82682fe94fe
dzgierski19 <dzgierskigit@gmail.com>
2023-07-22 19:46:08 +0200

    Products details added

--- Cart/src/app/Products.ts
+++ Cart/src/app/Products.ts
@@ -4,16 +4,39 @@ import { v4 as uuidv4 } from "uuid";
 import { DISCOUNTS } from "./Discounts";
 
 const levisMaleShoes = new Product(
-  "Levi's Jeans",
+  "Levi's shoes",
   CATEGORIES.MALE,
   uuidv4(),
   100
 );
 
-const wranglerFemaleShoes = new Product(
-  "Wrangler Jeans",
+const nikeFemaleShoes = new Product(
+  "Nike shoes",
   CATEGORIES.FEMALE,
   uuidv4(),
   125,
   DISCOUNTS.TWENTY_PERCENT_DISCOUNT
+);
+
+const adidasChildrenShoes = new Product(
+  "New Balance shoes",
+  CATEGORIES.CHILDREN,
+  uuidv4(),
+  125,
+  DISCOUNTS.FIFTY_PERCENT_DISCOUNT
+);
+
+const shoePolish = new Product(
+  "shoe polish",
+  CATEGORIES.ACCESSORIES,
+  uuidv4(),
+  30,
+  DISCOUNTS.THIRTY_PERCENT_DISCOUNT
+);
+
+const gucciShoes = new Product(
+  "Gucci shoes",
+  CATEGORIES.PREMIUM,
+  uuidv4(),
+  4000
 );
